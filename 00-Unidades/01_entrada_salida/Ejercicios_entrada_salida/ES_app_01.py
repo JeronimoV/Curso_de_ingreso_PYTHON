c050edda39ddb13ecce078c3066980dca6bbb63a
bobardou <jeronimoevilar@outlook.com>
2024-02-05 10:08:18 -0300

    nombe y apellido

--- 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
+++ 00-Unidades/01_entrada_salida/Ejercicios_entrada_salida/ES_app_01.py
@@ -5,8 +5,8 @@ from tkinter.simpledialog import askstring as prompt
 import customtkinter
 
 '''
-nombre:
-apellido:
+nombre: Jeronimo
+apellido: Vilar
 ---
 Ejercicio: entrada_salida_01
 ---
@@ -27,7 +27,6 @@ class App(customtkinter.CTk):
 
     def btn_mostrar_on_click(self):
         alert("E/S_01", "Esto no anda, funciona")
-
 
 if __name__ == "__main__":
     app = App()
